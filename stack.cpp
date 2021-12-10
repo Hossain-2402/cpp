a2fba5ba9e55210af1cbd99edddd317ea9b5dcdc
Hossain <hossain.ie2402@gmail.com>
2021-12-11 02:08:57 +0600

    CODE

--- stack.cpp
+++ stack.cpp
@@ -1,51 +1,116 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+#include<bits\stdc++.h>
+using namespace std;
 
-int partition (int arr[], int left, int right) { 
-	int pivot = arr[right]; 
-	int i = (left - 1);
-	for (int j = left; j < right; j++) 
-	{ 
-		if (arr[j] < pivot) 
-		{ 
-			i++;
-			int temp = arr[i];
- 			arr[i] = arr[j];
- 			arr[j] = temp;
-		} 
-	} 
-	int temp = arr[i+1];
-	arr[i+1] = arr[right];
-	arr[right] = temp;
-	return (i + 1); 
-} 
+struct Node{
+	int data;
+	Node *next;
+};
 
-void quickSort(int arr[], int left, int right) 
-{ 
-	if (left < right) 
-	{ 
-		int p = partition(arr, left, right); 
+vector<int> v;
+ 
+void prepend(Node **head,int new_data){
+	Node *new_node = (Node*) malloc(sizeof(Node));
+	new_node->data = new_data;
+	new_node->next = (*head);
+	(*head) = new_node; 
+}
+void append(Node **head,int new_data){
+	Node *new_node = (Node*) malloc(sizeof(Node));
+	Node *last_element = *head;
+	
+	new_node->data = new_data;
+	new_node->next = NULL;
 
-		quickSort(arr, left, p - 1); 
-		quickSort(arr, p + 1, right); 
-	} 
-} 
+	if(*head == NULL){
+		*head = new_node;
+		return;
+	}
+	while(last_element->next != NULL){
+		last_element = last_element->next;
+	}
+	last_element->next = new_node;  
+	return;
+}
+bool search(Node *node,int info){
+	while(node != NULL){
+		if(node->data == info){
+			return true;
+		}
+		node = node->next;
+	}
+	return false;
+}
+void printTheLinkedList(Node *node){
+	while(node != NULL){
+		cout << " " << node->data;
+		v.push_back(node->data);
+		node = node->next;
+	}
+}
+void reverse(){ 
+	int left = 0;
+	int right = v.size() - 1;
+	while(left < right){
+		int temp = v[left];
+		v[left] = v[right];
+		v[right] = temp;
+		left++;
+		right--;
+	}
+	for(int i=0;i<=(v.size() - 1);i++){
+		cout << " " << v[i];
+	}
+}
+void sortIt(){
+	for(int i=0;i<=(v.size() - 1);i++){
+		int largest = i;
+		for(int j=i+1;j<=(v.size() - 1);j++){
+			if(v[j] > v[largest]){
+				largest = j;
+			}
+		}
+		if(largest != i){
+			int temp = v[largest];
+			v[largest] = v[i];
+			v[i] = temp;
+		}
+	}
+	for(int i=0;i<=(v.size() - 1);i++){
+		cout << " " << v[i];
+	}
+}
 
-void printArray(int arr[], int size) 
-{ 
-	int i; 
-	for (i = 0; i < size; i++) 
-		cout << arr[i] << " "; 
-	cout << endl; 
-} 
+int main(){
+	Node *head = NULL;
+	append(&head,2);
+	append(&head,5);
+	append(&head,4);
+	append(&head,3);
+	append(&head,7);
+	append(&head,6);
+	append(&head,10);
+	append(&head,9);
+	append(&head,8);
+	prepend(&head,1);
+	
+	cout << endl ;
+	printTheLinkedList(head);
+	cout << endl ;
 
-int main() 
-{ 
-	int arr[] = {5,4,3,2,1}; 
-	int n = 5;
-	quickSort(arr, 0, n - 1); 
-	cout << endl <<"Sorted array: "; 
-	printArray(arr, n); 
-	return 0; 
-} 
+	bool searching = search(head,10);
+	if(searching == 1){
+		cout << endl << " TRUE" << endl;
+	}
+	else{
+		cout << endl << " FALSE" << endl;
+	}
 
+	cout << endl;
+	reverse();
+	cout << endl;
+
+
+	cout << endl;
+	sortIt();
+	cout << endl;
+}
