f878891d6fcd646ea3e0051df2e19e7e99aa5d8d
Hossain <hossain.ie2402@gmail.com>
2022-01-01 20:23:14 +0600

    CODE

--- ran.cpp
+++ ran.cpp
@@ -1,8 +1,19 @@
 #include<bits\stdc++.h>
 using namespace std;
 
+#define FOR(i,a) for(int i=0;i<a;i++)
+#define sz(a) a.size()
+typedef vector<int> vi;
+const string nl = "\n";
 
 
 int main(){
+
+	vi ans = {1,2,3,4,6};
+	int i = 0;
+
+	cout << nl;
+	FOR(i,sz(ans)) cout << " " << ans[i] ;
+	cout << nl;
 
 }
